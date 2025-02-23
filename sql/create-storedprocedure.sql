d4f2edec0ef9818935c76fea01169bab72dfcb9e
Hemanth <trhemanth2@gmail.com>
2025-02-23 20:57:50 +0530

    modify scripts for sql-server

--- sql/create-storedprocedure.sql
+++ sql/create-storedprocedure.sql
@@ -1,85 +1,76 @@
 
-CREATE OR REPLACE PROCEDURE CreateUser (
-	UserId VARCHAR(30),
-	Fullname VARCHAR(100),
-	Birthdate DATE,
-	Email VARCHAR(100),
-	Phone VARCHAR(10)
-)
-LANGUAGE plpgsql AS
-$$
-DECLARE
-	IsUserExists BOOLEAN;
+CREATE OR ALTER PROCEDURE [dbo].[CreateUser]
+	-- Add the parameters for the stored procedure here
+	@Username VARCHAR(30),
+	@Fullname VARCHAR(100),
+	@Birthdate DATE,
+	@Email VARCHAR(100),
+	@Phone VARCHAR(10) = NULL
+AS
 BEGIN
-	SELECT EXISTS(SELECT * FROM "user" WHERE "Username" = UserId) INTO IsUserExists;
 
-	IF IsUserExists THEN
-    	RAISE EXCEPTION 'ALREADY EXISTS';
-	ELSE
-		INSERT INTO "user"
-		VALUES (gen_random_uuid(), UserId, Fullname, Birthdate, Email, Phone);
-		RAISE NOTICE 'CREATED';
-	END IF;
+	IF NOT EXISTS (SELECT * FROM Users WHERE Username = @Username)
+	BEGIN
+		INSERT INTO Users (Username, Fullname, Birthdate, Email, Phone)
+		VALUES (@Username, @Fullname, @Birthdate, @Email, @Phone);
+	END
+
 END
-$$;
 
-CREATE OR REPLACE PROCEDURE CreateTrade(
-	IN stockname character varying,
-	IN ep numeric,
-	IN username character varying)
-LANGUAGE 'plpgsql'
-AS $BODY$
-DECLARE	
-	IsUserExists BOOLEAN;
-	IsTradeExists BOOLEAN;
-	UId UUID;
-	TId UUID;
+CREATE OR ALTER PROCEDURE CreateTrade
+	-- Add the parameters for the stored procedure here
+	@StockName VARCHAR(20),
+	@EntryPrice INT,
+	@Username VARCHAR(100)
+AS
 BEGIN
 
-	SELECT EXISTS(SELECT * FROM "user" WHERE "Username" = username) INTO IsUserExists;
+	DECLARE @UserGuid UNIQUEIDENTIFIER
+	DECLARE @NewTradeId UNIQUEIDENTIFIER
 
-	IF IsUserExists THEN
-
-		SELECT Id INTO UId FROM "user" WHERE Username = username;
-		
-		SELECT EXISTS(SELECT * FROM Trade WHERE Id IN (SELECT TradeId FROM Trades WHERE UserId = UId) AND Stock = stockname AND Entryprice = ep AND Exitprice is null) INTO IsTradeExists;
-
-		IF IsTradeExists <> 't' THEN
-
-			TId = gen_random_uuid();
-			INSERT INTO Trade
-			VALUES (TId, Stockname, ep, null, CURRENT_TIMESTAMP, NULL);
-
+	IF EXISTS (SELECT * FROM Users WHERE Username = @Username)
+	BEGIN
+		SELECT @UserGuid = Id FROM Users WHERE Username = @Username
+		IF NOT EXISTS (SELECT * FROM Trades WHERE Id IN (SELECT TradeId FROM UserTradeMap WHERE UserId = @UserGuid) AND Stock = @StockName AND Entryprice = @EntryPrice AND Exitprice IS NULL)
+		BEGIN
+			SET @NewTradeId = NEWID()
 			INSERT INTO Trades
-			VALUES (TId, UId);
-			RAISE NOTICE 'CREATED';
+			VALUES (@NewTradeId, @StockName, @EntryPrice, NULL, CURRENT_TIMESTAMP, NULL)
 
+			INSERT INTO UserTradeMap
+			VALUES (@NewTradeId, @UserGuid)
+		END
 		ELSE
-
-			RAISE EXCEPTION 'TRADE EXIST'
-			
-		END IF;
-		
+		BEGIN
+			RAISERROR('TRADE DONOT EXISTS',10,1) WITH NOWAIT;
+		END
+	END
 	ELSE
-		RAISE EXCEPTION 'USER DOENST EXIST';
-	END IF;
-END
-$$;
+	BEGIN
+		RAISERROR('USER DONOT EXISTS',10,1) WITH NOWAIT;
+	END
 
-CREATE OR REPLACE FUNCTION GetTrades(
-	Uname VARCHAR(30)
-)
-RETURNS TABLE ("Id" UUID, Stock VARCHAR, Entryprice DECIMAL, Exitprice DECIMAL, Entrydatetime TIMESTAMP, Exitdatetime TIMESTAMP) AS $$
-DECLARE
-	IsUserExists BOOLEAN;
-	UId UUID;
+END
+GO
+
+
+CREATE OR ALTER PROCEDURE GetTrades
+	-- Add the parameters for the stored procedure here
+	@Username VARCHAR(30)
+AS
 BEGIN
-	SELECT EXISTS(SELECT * FROM "user" WHERE "Username" = Uname) INTO IsUserExists;
-	IF IsUserExists THEN
-		SELECT Id INTO UId FROM "user" WHERE "Username" = Uname;
-		RAISE NOTICE '%', UId;
-    	RETURN QUERY 
-    	SELECT * FROM TRADE WHERE Id IN (SELECT Id FROM TRADES WHERE UserId = UId);
-	END IF;
-END;
-$$ LANGUAGE plpgsql;
+
+	DECLARE @UserGuid UNIQUEIDENTIFIER
+
+	IF EXISTS (SELECT * FROM Users WHERE Username = @Username)
+	BEGIN
+		SELECT @UserGuid = Id FROM Users WHERE Username = @Username
+		SELECT * FROM Trades WHERE Id IN (SELECT TradeId FROM UserTradeMap WHERE UserId = @UserGuid)
+	END
+	ELSE
+	BEGIN
+		RAISERROR('USER DONOT EXISTS',10,1) WITH NOWAIT;
+	END
+
+END
+GO
